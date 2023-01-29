1d92c4c207450983d7079e19721a6de8450639af
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-29 20:40:45 -0300

    install express

--- Node/src/index.ts
+++
@@ -1,9 +0,0 @@
-import validator from "validator";
-
-let name: string = 'dayanne freitas';
-
-if(validator.isLowercase(name)) {
-    console.log(`A String ${name} está toda minúscula`)
-} else {
-    console.log(`A String ${name} NÃO É toda minúscula`)
-};
