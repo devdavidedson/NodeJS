59991b9cc67c0747c82bd620b97a9fa808672df0
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-29 19:36:45 -0300

    nodemon libs ts

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -1,6 +1,6 @@
 import validator from "validator";
 
-let name: string = 'dayanne';
+let name: string = 'dayanne freitas';
 
 if(validator.isLowercase(name)) {
     console.log(`A String ${name} está toda minúscula`)
