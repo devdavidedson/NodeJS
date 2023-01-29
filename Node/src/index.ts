884ea0e03085117b4f1269e8068c9b440b70dd9c
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-29 18:17:55 -0300

    import bibliotecas terceiras

--- Node/src/index.ts
+++ Node/src/index.ts
@@ -1,7 +1,3 @@
-import Matematica from "./Matematica";
+import validator from "validator";
 
-let n1: number = 10;
-let n2: number = 2;
-
-console.log(`SOMAR ${Matematica.somar(n1, n2)}`)
-console.log(`SUBTRAÇÃO ${Matematica.subtrair(n1, n2)}`)
+console.log(validator.isEmail("davidsvce@gmail.com"))
