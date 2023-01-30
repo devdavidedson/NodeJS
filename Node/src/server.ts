bb7aaa647726dad12d0dc0e6bdd0f8ea64cdac92
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-30 18:05:37 -0300

    Page not Found Route

--- Node/src/server.ts
+++ Node/src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import mainRoutes from './Routes/index';
-import mainPainel from './Routes/painel';
 
 const server = express();
 
-server.use('/', mainRoutes);
-server.use('/painel',mainPainel);
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response) => {
+    res.status(404).send('Page not Found!')
+});
 
 server.listen(80);
