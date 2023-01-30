cfc7d9f42b3718c439e4737c6dbcf9763b984ef3
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-30 12:08:09 -0300

    teoria de rotas

--- Node/src/server.ts
+++ Node/src/server.ts
@@ -2,8 +2,25 @@ import express, {Request, Response} from 'express';
 
 const server = express();
 
+// GET, POST, PUT, DELETE são os metodos mais usados
+
 server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!');
+})
+
+//Exemplo de rota Dinamica
+
+server.get('/noticia/:slug', (req: Request, res: Response) => {
+    let slug: string = req.params.slug;
+    res.send(`Noticia: ${slug}`);
+})
+
+// Exemplo de rota Dinamica 2 elementos
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response) => {
+    
+    let {origem, destino} = req.params;
+    res.send(`Passagens Aereas de ${origem.toUpperCase()} para ${destino.toUpperCase()}`)
 })
 
 server.listen(80)
