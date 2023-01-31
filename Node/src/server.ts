ecf0f60c4c0ac3c983584a77dddf37a6a3d1dbc9
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-31 09:35:48 -0300

    instalando e configurando mustache

--- Node/src/server.ts
+++ Node/src/server.ts
@@ -1,10 +1,16 @@
 import express, {Request, Response} from 'express';
 import path from 'path';
+
+import mustache from 'mustache-express';
 import mainRoutes from './Routes/index';
 
 const server = express();
 
-server.use(express.static(path.join(__dirname,'../public')))
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname,'../public')));
 
 server.use(mainRoutes);
 
