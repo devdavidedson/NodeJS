a29edfcc6c76c764685f2ed812c55e818adc0a6b
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-31 09:49:17 -0300

    exibindo informações mustache

--- Node/src/Routes/index.ts
+++ Node/src/Routes/index.ts
@@ -3,7 +3,14 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.render('home')
+    let user = {
+        name: 'Dayanne',
+        age: 90
+    }
+    
+    res.render('home', {
+        user
+    })
 });
 
 router.get('/contato', (req: Request, res: Response) => {
