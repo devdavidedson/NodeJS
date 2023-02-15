dcfe44d5c3ed0965c847fab58c23c9faf3ab8b7b
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-02-15 19:49:32 -0300

    alterações de usuario 1

--- PostgreSQL/src/controllers/homeController.ts
+++ PostgreSQL/src/controllers/homeController.ts
@@ -2,8 +2,26 @@ import { Request, Response } from 'express';
 
 import { User } from '../models/User';
 import { Product } from '../models/Product';
+import { Op } from 'sequelize';
 
 export const home = async (req: Request, res: Response)=>{
+    // alterando muitos usuários
+    await User.update({ age: 18 },{
+        where: {
+            age: {
+                [Op.lt]: 18
+            }
+        }
+    });
+    // Alterando um só
+    await User.update({ name: 'Seu David Edson', age: 33 }, {
+        where: {
+            id: 4
+            }
+        });
+   
+   
+   
     const users = await User.findAll();
 
     let age: number = 90;
